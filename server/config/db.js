91c0d51bf8e708be206a6819b98749d2ee9ee890
Prince0408 <princearora0408@gmail.com>
2020-10-29 20:17:20 -0400

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+//======Created by Prince========301105083
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://Prince:46Uz6Vowre7TwywE@mongodbserver.p7cro.mongodb.net/books?retryWrites=true&w=majority"
+};
---
+++ server/models/books.js
@@ -0,0 +1,16 @@
+//======Created by Prince========301105083
+let mongoose = require('mongoose');
+
+// create a model class
+let Book = mongoose.Schema({
+    Title: String,
+    Description: String,
+    Price: Number,
+    Author: String,
+    Genre: String
+},
+{
+  collection: "books"
+});
+
+module.exports = mongoose.model('Book', Book);
